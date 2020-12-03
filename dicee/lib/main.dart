0127f2c42be4191b76bb040107146db48c1a10fa
FrankJoseph <frankjoseph.serdenia.530@my.csun.edu>
2020-12-03 15:16:00 -0800

    Finished dice project

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,23 +1,80 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//Expanded widget -> allows the child to adapt on the size of the parent widget
+//Stateless widget -> if the structure of the context does not change, this is preferred
+//Stateful widget -> if the structure of the context does change, this is preferred
 void main() {
-  return runApp(
+  runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
+        backgroundColor: Colors.grey,
         body: DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void changeDice() {
+    setState(() {
+      leftDiceNumber = 1 + Random().nextInt(6);
+      rightDiceNumber = 1 + Random().nextInt(6);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Dice Roller',
+              style: GoogleFonts.blackOpsOne(
+                  color: Colors.lightBlueAccent[700],
+                  fontSize: 60,
+                  letterSpacing: 5,
+                  height: 4.0),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(100.0),
+              child: FloatingActionButton(
+                onPressed: changeDice,
+                child: Icon(Icons.whatshot_rounded),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
