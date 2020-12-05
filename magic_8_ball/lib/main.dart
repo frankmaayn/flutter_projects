4445e8168b194098ad6b1ec0a61215e085e62b5c
FrankJoseph <frankjoseph.serdenia.530@my.csun.edu>
2020-12-04 18:07:31 -0800

    Added new features

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -13,9 +14,6 @@ class BallPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey,
-      appBar: AppBar(
-        title: Text('Ask Me Anything'),
-      ),
       body: Ball(),
     );
   }
@@ -37,11 +35,55 @@ class _BallState extends State<Ball> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: FlatButton(
-          onPressed: getAnswer,
-          child: Image.asset('images/ball$ballNumber.png'),
+    return SafeArea(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 5),
+              child: Text(
+                "Magic 8 Ball",
+                style: GoogleFonts.bowlbyOneSc(
+                  color: Colors.blueAccent,
+                  fontSize: 55,
+                  letterSpacing: 2,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 250,
+              child: Divider(
+                thickness: 2,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "Ask me a question",
+                style: GoogleFonts.pridi(fontSize: 30),
+              ),
+            ),
+            Container(
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 60),
+              child: Container(
+                color: Colors.blue,
+                child: FlatButton(
+                  onPressed: getAnswer,
+                  child: Text(
+                    'Press me',
+                    style: GoogleFonts.bungee(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
